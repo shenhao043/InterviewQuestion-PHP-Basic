30790508be463026f42cf30bc8db6a87f67d631d
shenhao043 <82090803+shenhao043@users.noreply.github.com>
2022-09-27 21:03:05 +0800

    Update html.php

---
+++ html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>CSS Button Style</title>
+</head>
+<body>
+    <button type="button" class="button">Submit
+    </button>
+    
+<style>
+.button{
+	background-color: #4CAF50;
+    color: #FDFEFE;
+    }
+
+</body>
+</html>
